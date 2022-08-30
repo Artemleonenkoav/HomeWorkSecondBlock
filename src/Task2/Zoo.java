df58c3fabd72707b99d783a2ef78d04408b40ca7
Artem <leonenkoavfin@gmail.com>
2022-08-30 19:02:05 +0300

    Add code Task 2

---
+++ src/Task2/Zoo.java
@@ -0,0 +1,20 @@
+//Используя класс Zoo  удалить 2  животныx, узнать размер списка и вывести в консоль.
+
+package Task2;
+
+import java.util.ArrayList;
+
+public class Zoo {
+    public static void main(String[] args) {
+            ArrayList<String> arrayList = new ArrayList<>();
+            arrayList.add(0, "Dog");
+            arrayList.add(1, "Cat");
+            arrayList.add(2, "Wild");
+            arrayList.add(3, "Hors");
+
+            arrayList.remove(1);
+            arrayList.remove(2);
+
+            System.out.println(arrayList);
+        }
+    }
