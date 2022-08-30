a59d25127ae85346c2619f730fe9f5b5f1c3f83d
Artem <leonenkoavfin@gmail.com>
2022-08-30 19:06:34 +0300

    Change pool

--- src/Task2/Zoo.java
+++ src/Task2/Zoo.java
@@ -6,15 +6,13 @@ import java.util.ArrayList;
 
 public class Zoo {
     public static void main(String[] args) {
-            ArrayList<String> arrayList = new ArrayList<>();
-            arrayList.add(0, "Dog");
-            arrayList.add(1, "Cat");
-            arrayList.add(2, "Wild");
-            arrayList.add(3, "Hors");
-
-            arrayList.remove(1);
-            arrayList.remove(2);
-
-            System.out.println(arrayList);
-        }
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add(0, "Dog");
+        arrayList.add(1, "Cat");
+        arrayList.add(2, "Wild");
+        arrayList.add(3, "Hors");
+        arrayList.remove(1);
+        arrayList.remove(2);
+        System.out.println(arrayList);
     }
+}
