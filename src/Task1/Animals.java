69ea36d444ff44a7157c065be10a078ad0329bc5
Artem <leonenkoavfin@gmail.com>
2022-08-31 17:00:35 +0300

    Add  code

---
+++ src/Task1/Animals.java
@@ -0,0 +1,29 @@
+// + Создайте перечислительный тип (enum) Animals, содержащий конструктор, который должен принимать
+// + целочисленное значение (возраст животного), и содержать перегруженный метод toString(), который
+// + должен возвращать название экземпляра и возраст животного.
+
+package Task1;
+
+enum Animals {
+    DOG(50), CAT(30), TIGER(55);
+    int age;
+
+    Animals(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Animals animals = Animals.DOG;
+        Animals animalse = Animals.CAT;
+        Animals animalss = Animals.TIGER;
+        System.out.println("Age animal is " + animals.age);
+        System.out.println("Age animal is " + animalse.age);
+        System.out.println("Age animal is " + animalss.age);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
