5058032181750e292b5fe657107d65e3c06e9717
Artem <leonenkoavfin@gmail.com>
2022-08-31 11:12:18 +0300

    Change Converter

---
+++ src/Task1/Vehicle.java
@@ -0,0 +1,27 @@
+// Создать класс Vehicle с методом print. В классе Vehicle, создайте внутренний класс Wheel и Door, которые
+// также должны содержать метод print. Создайте экземпляры классов Wheel и Door.
+
+package Task1;
+
+public class Vehicle {
+
+    class Wheel {
+        Wheel myWheel = new Wheel();
+
+        public void print() {
+
+        }
+    }
+
+    class Door {
+        Door myDoor = new Door();
+
+        public void print() {
+
+        }
+    }
+
+    public void print() {
+
+    }
+}
